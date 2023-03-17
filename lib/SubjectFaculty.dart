b6bf26b2f0a6cbb2150832ae6185b62765bca295
Vaibhav Patil <89802252+03patilvaibhav@users.noreply.github.com>
2023-03-17 19:48:38 +0530

    Src Folder

---
+++ lib/SubjectFaculty.dart
@@ -0,0 +1,12 @@
+
+class SubjectFaculty {
+  String subject_name="";
+  String faculty_name="";
+
+
+  SubjectFaculty(sub,faculty){
+    this.subject_name = sub;
+    this.faculty_name = faculty;
+
+  }
+}
